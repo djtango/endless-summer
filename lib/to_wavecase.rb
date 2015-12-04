821bf930098e286b74f898ecd5640ccdc0e8b81f
Deon Tan <deonjtan@gmail.com>
2015-12-04 11:47:35 +0000

    one liner

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,17 +1,5 @@
 class String
-  # def to_wavecase
-  #   self.split(' ').map{|word| word.split('').map.with_index{|char, i| i % 2 == 1 ? char.upcase : char.downcase}.join}.join(' ')
-
-  # end
-
   def to_wavecase
-    array = []
-    i = 0
-    self.split('').each{|char|
-      i % 2 == 0 ? char.downcase! : char.upcase!
-      i += 1 if char != ' '
-      array << char
-    }
-    array.join
+    self.gsub(/[[:alnum:]]/).with_index {|char, index| index.odd? ? char.upcase : char.downcase}
   end
 end
